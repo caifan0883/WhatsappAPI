90dcc0a2aae20bfb009835ee103bb49c0863de86
caifan0883 <gibranlc@gmail.com>
2025-03-22 00:28:55 -0700

    Agregando funcionalidad mensaje log y BD

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import json
 
 app = Flask(__name__)
 
@@ -12,20 +13,56 @@ db=SQLAlchemy(app)
 #Modelo de la tabla
 class Log(db.Model):
     id = db.Column(db.Integer,primary_key=True)
-    fecha_y_hora = db.Column(db.DateTime, default=datetime.timezone.utc)
+    fecha_y_hora = db.Column(db.DateTime, default=datetime.utcnow)
     texto = db.Column(db.TEXT)
 
 #Crear una table si no existe
 with app.app_context():
     db.create_all()
 
+
+
+#Funcion para ordenar los registros por Fecha y Hora
+def ordenar_por_fecha_y_hora(registros):
+    return sorted(registros, key=lambda x: x.fecha_y_hora, reverse=True)
+
 @app.route('/')
 
 def index():
     #obtener todos los registros de la base de datos
     registros = Log.query.all()
-    return render_template('index.html', registros=registros)
+    registros_ordenados = ordenar_por_fecha_y_hora(registros)
+    return render_template('index.html', registros=registros_ordenados)
 
+
+#Funcion para agregar mensajes y guardar en la base de datos
+mesajes_log = []
+def agregar_mensajes_log(texto):
+    mesajes_log.append(texto)
+
+    #Guardar mensajes en la BD
+    nuevo_registro = Log(texto=texto)
+    db.session.add(nuevo_registro)
+    db.session.commit
+
+#Token de configuracion para la verificacion
+TOKE_WHATSAPP_API = "PCU"
+
+@app.route('/webhook', methods=['GET','POST'])
+def webhook():
+    if request.method == 'GET':
+        challenge = verificar_token(request)
+        return challenge
+    elif request.method == 'POST':
+        response = recibir_mensajes(request)
+        return response
+
+
+def verificar_token(req):
+    return 0
+
+def recibir_mensajes(req):
+    return 0
 
 if (__name__)=='__main__':
     app.run(host='0.0.0.0',port=80,debug=True)
